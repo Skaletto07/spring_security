42ca36826bc7c21d666e90fcdf00ea393017d75f
Skaletto07 <Skaletto07@yandex.ru>
2022-06-21 18:29:09 +0300

    authorization

--- src/main/java/com/kostkin/spring/security/configuration/MyConfig.java
+++ src/main/java/com/kostkin/spring/security/configuration/MyConfig.java
@@ -1,12 +1,16 @@
 package com.kostkin.spring.security.configuration;
 
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 
 @Configuration
 @ComponentScan("com.kostkin.spring.security")
@@ -18,6 +22,20 @@ public class MyConfig {
 
         InternalResourceViewResolver resolver = new InternalResourceViewResolver("/WEB-INF/view/",".jsp");
         return resolver;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false");
+            dataSource.setUser("bestuser");
+            dataSource.setPassword("bestuser");
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+        return dataSource;
     }
 
 }
--- src/main/webapp/WEB-INF/view/view_for_all_employees.jsp
+++ src/main/webapp/WEB-INF/view/view_for_all_employees.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="security" uri="http://www.springframework.org/security/tags" %>
 <%--
   Created by IntelliJ IDEA.
   User: tamyk
@@ -15,12 +16,18 @@
   <h3>Information for all employees</h3>
 <br>
 <br>
-  <input type="button" value="Salary" onclick="window.location.href = 'hr_info'">
- Only for HR staff
+  <security:authorize access="hasRole('HR')">
+      <input type="button" value="Salary" onclick="window.location.href = 'hr_info'">
+      Only for HR staff
+  </security:authorize>
+
 <br>
 <br>
-  <input type="button" value="Performance" onclick="window.location.href = 'manager_info'">
- Only for managers
+  <security:authorize access="hasRole('MANAGER')">
+      <input type="button" value="Performance" onclick="window.location.href = 'manager_info'">
+      Only for managers
+  </security:authorize>
+
 
 </body>
 </html>
